17f28fde28038b893db14e1a793973cc0a740a7f
Manas Raj <manasraj1357@gmail.com>
2025-01-18 12:38:16 +0530

    .

--- Frontend/src/components/Navbars/SellerNavbar.jsx
+++ Frontend/src/components/Navbars/SellerNavbar.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import { IoMdAddCircle } from "react-icons/io";
 import { MdAccountCircle } from "react-icons/md";
+import { PiShoppingCartSimpleThin } from "react-icons/pi";
 
 const SellerNavbar = () => {
   return (
     <nav className='bg-black text-white'>
       <div className="mycontainer px-4 py-3 flex h-14 items-center justify-between">
         {/* Logo */}
-        <div className="logo">
-          <img className="max-w-[200px] max-h-auto" src="/icons/SwiftShopper.svg" alt="SwiftShopper Logo" />
-        </div>
+       <div className='relative flex items-center gap-1'>
+                 <PiShoppingCartSimpleThin className='text-4xl text-red-600'/>
+                 <h1 className="text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-600 to-pink-500 shadow-xl transform transition-all duration-500 hover:scale-110">
+                 Zylokart</h1>
+               </div>
 
         {/* Links */}
         <div>
